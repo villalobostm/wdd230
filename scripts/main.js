539bd3bd8a3466b98d289136bb34313cdbc359d4
villalobostm <thomasvillalobos@gmail.com>
2022-09-20 01:16:20 -0600

    index

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
-// toggle menu in small view
-const toggleMenu = () => {
-    document.querySelector('#menu').classList.toggle('open');
-}
+let current_date = new Date();
 
-document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
+let current_year = current_date.getFullYear();
 
+document.querySelector('#year').innerHTML = current_year;
+
+let oLastModif = new Date(document.lastModified);
+
+var options = {year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false};
+
+oLastModif = oLastModif.toLocaleDateString("en-US", options); 
+
+document.querySelector('#lastMod').innerHTML = oLastModif;
