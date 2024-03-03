221c57886a7e95c8c72d80d02fe855bbea221cd3
villalobostm <thomasvillalobos@gmail.com>
2024-03-02 18:07:15 -0600

    wind chill calculator

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -8,7 +8,8 @@ input_temp.addEventListener('change', () => {
 
         if (input_temp.value <= 50 && input_wind.value >= 3) {
 
-            oWindChill = 35.74 + 0.6215 * input_temp.value - 35.75 * input_wind.value ^ (0.16) + 0.4275 * input_temp.value * input_wind.value ^ (0.16);
+            oWindChill = 35.74 + 0.6215 * input_temp.value - 35.75 * input_wind.value ** 0.16 + 0.4275 * input_temp.value * input_wind.value ** 0.16;
+            oWindChill = Math.round(oWindChill);
             document.querySelector('#WindChill').innerHTML = oWindChill;
 
         } else {
@@ -32,7 +33,8 @@ input_wind.addEventListener('change', () => {
 
         if (input_temp.value <= 50 && input_wind.value >= 3) {
 
-            oWindChill = 35.74 + 0.6215 * input_temp.value - 35.75 * input_wind.value ^ (0.16) + 0.4275 * input_temp.value * input_wind.value ^ (0.16);
+            oWindChill = 35.74 + 0.6215 * input_temp.value - 35.75 * input_wind.value ** 0.16 + 0.4275 * input_temp.value * input_wind.value ** 0.16;
+            oWindChill = Math.round(oWindChill);
             document.querySelector('#WindChill').innerHTML = oWindChill;
 
         } else {
